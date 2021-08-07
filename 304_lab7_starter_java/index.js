ce16804a680c0e8284ca2c0fb392f73711024c3c
avias8 <HarshvardhanV98@gmail.com>
2021-08-07 12:46:50 -0600

    Some js work

---
+++ 304_lab7_starter_java/index.js
@@ -0,0 +1,31 @@
+const sql = require('mssql')
+const { PassThrough } = require('stream')
+
+const config = {
+    user: 'sa',
+    password: 'YourStrong@Passw0rd',
+    server: 'localhost',
+    database: 'tempdb',
+    options: {
+        enableArithAbort: true
+    }
+}
+
+const run = async () => {
+    try {
+        console.log('Connection Opening...');
+        const pool = await sql.connect(config);
+        const { recordset } = await sql.query`select * from users;`;
+
+        console.log(recordset);
+    } catch (err) {
+        console.log(err);
+    } finally {
+        await pool.close();
+        console.log('Connection closed');
+    }
+
+}
+
+run();
+
