d00a434a1b636e00a6a41f1bea48a52eacf8cc48
avias8 <HarshvardhanV98@gmail.com>
2021-08-07 20:47:23 -0600

    a

--- 304_lab7_starter_java/WebContent/AjaxTest/gethint.php
+++ 304_lab7_starter_java/WebContent/AjaxTest/gethint.php
@@ -20,7 +20,7 @@ $a[] = "Raquel";
 $a[] = "Cindy";
 $a[] = "Doris";
 $a[] = "Eve";
-$a[] = "Evita";
+$a[] = "Evita"; 
 $a[] = "Sunniva";
 $a[] = "Tove";
 $a[] = "Unni";
@@ -30,27 +30,23 @@ $a[] = "Elizabeth";
 $a[] = "Ellen";
 $a[] = "Wenche";
 $a[] = "Vicky";
-
 // get the q parameter from URL
 $q = $_REQUEST["q"];
-
 $hint = "";
-
 // lookup all hints from array if $q is different from ""
 if ($q !== "") {
-  $q = strtolower($q);
-  $len=strlen($q);
-  foreach($a as $name) {
-    if (stristr($q, substr($name, 0, $len))) {
-      if ($hint === "") {
-        $hint = $name;
-      } else {
-        $hint .= ", $name";
-      }
-    }
-  }
+ $q = strtolower($q);
+ $len=strlen($q);
+ foreach($a as $name) {
+ if (stristr($q, substr($name, 0, $len))) {
+ if ($hint === "") {
+ $hint = $name;
+ } else {
+ $hint .= ", $name";
+ }
+ }
+ }
 }
-
 // Output "no suggestion" if no hint was found or output correct values
 echo $hint === "" ? "no suggestion" : $hint;
-?>
+?> 
